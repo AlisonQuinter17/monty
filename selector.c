b3beb87c6c53b0df922e581abfb3467dec1bd491
julianamonr03 <julianamonroy03@hotmail.com>
2020-09-17 22:59:51 +0000

    Function selector for execute function

---
+++ selector.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+
+/**
+ * selector - Function for select parameter op.
+ * @stack: Double pointer to the head of the Stack.
+ * @tokens: Entry parameter.
+ * @count: Number of the line.
+ * Return: Nothing.
+ */
+void selector(stack_t **stack, char *tokens, unsigned int count)
+{
+	instruction_t functions[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"swap", swap},
+		{"add", add},
+		{"sub", sub},
+		{"nop", nop},
+		{"pop", pop},
+		{NULL, NULL}
+	};
+
+	int format = 0; /* Variable for travel the structure*/
+
+	while (format < 8)
+	{
+		/* Compare the entry parameter with the functions for execute*/
+		if (strcmp(functions[format].opcode, tokens) == 0)
+		{
+			functions[format].f(stack, count);
+		}
+		format++;
+	}
+}
