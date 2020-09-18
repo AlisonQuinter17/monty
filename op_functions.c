7ec2b622d4136f14ecd54ac5df715db281907062
julianamonr03 <julianamonroy03@hotmail.com>
2020-09-18 03:40:14 +0000

    Fix files structure

--- op_functions.c
+++ op_functions.c
@@ -24,63 +24,18 @@ void swap(stack_t **stack, unsigned int count)
 }
 
 /**
- * add - This function adds the top two elements of the stack.
+ * mod - computes the rest of the division of the
+ * second top element of the stack by the top element of the stack.
  * @stack: Double pointer to the head of the Stack.
  * @count: Number of the line.
  *
  * Return: Nothing.
  **/
-void add(stack_t **stack, unsigned int count)
-{
-	int sum;
-
-	if (!stack || !(*stack) || !(*stack)->next)
-	{
-		fprintf(stderr, "L%u: can't add, stack too short\n", count);
-		free(stack);
-		exit(EXIT_FAILURE);
-	}
-	sum = (*stack)->n;
-	sum += (*stack)->next->n;
-	pop(stack, count);
-	(*stack)->n = sum;
-}
-
-/**
- * sub - subtracts the top element of the stack
- * from the second top element of the stack.
- * @stack: Double pointer to the head of the Stack.
- * @count: Number of the line.
- *
- * Return: Nothing.
- **/
-void sub(stack_t **stack, unsigned int count)
+void mod(stack_t **stack, unsigned int count)
 {
 	if (!stack || !(*stack) || !(*stack)->next)
 	{
-		fprintf(stderr, "L%u: can't sub, stack too short\n", count);
-		exit(EXIT_FAILURE);
-	}
-
-	(*stack)->next->n -= (*stack)->n;
-	(*stack) = (*stack)->next;
-	free((*stack)->prev);
-	(*stack)->prev = NULL;
-}
-
-/**
- * _div - This function subtracts the top element of the stack from
- * the second top element of the stack.
- * @stack: Double pointer to the head of the Stack.
- * @count: Number of the line.
- *
- * Return: Nothing.
- **/
-void _div(stack_t **stack, unsigned int count)
-{
-	if (!stack || !(*stack) || !(*stack)->next)
-	{
-		fprintf(stderr, "L%u: can't div, stack too short\n", count);
+		fprintf(stderr, "L%u: can't mod, stack too short\n", count);
 		exit(EXIT_FAILURE);
 	}
 
@@ -90,29 +45,7 @@ void _div(stack_t **stack, unsigned int count)
 		exit(EXIT_FAILURE);
 	}
 
-	(*stack)->next->n /= (*stack)->n;
-	(*stack) = (*stack)->next;
-	free((*stack)->prev);
-	(*stack)->prev = NULL;
-}
-
-/**
- * mul - This function subtracts the top element of the stack from
- * the second top element of the stack.
- * @stack: Double pointer to the head of the Stack.
- * @count: Number of the line.
- *
- * Return: Nothing.
- **/
-void mul(stack_t **stack, unsigned int count)
-{
-	if (!stack || !(*stack) || !(*stack)->next)
-	{
-		fprintf(stderr, "L%u: can't mul, stack too short\n", count);
-		exit(EXIT_FAILURE);
-	}
-
-	(*stack)->next->n *= (*stack)->n;
+	(*stack)->next->n %= (*stack)->n;
 	(*stack) = (*stack)->next;
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
